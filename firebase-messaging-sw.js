651b6b46d1045bf6aba649703fb1f2cd95e51159
Shariq Ali Khan <38683828+shariqkhanyousufzai@users.noreply.github.com>
2021-05-21 07:41:23 -0700

    background handler

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js');
 
 var firebaseConfig = {
     apiKey: "AIzaSyDzIIy78Kc1Da9vJYwpjhu8L1xGS05abiM",
@@ -22,4 +22,17 @@ messaging.setBackgroundMessageHandler(function (payload) {
         icon:notification.icon
     };
     return self.registration.showNotification(payload.notification.title,notificationOption);
+});
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
